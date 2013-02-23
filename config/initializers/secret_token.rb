d15ed4f0d421ca1d586c310bbd862e246c135c17
Maximilian Schulz <m.schulz@kulturfluss.de>
2013-02-23 12:10:54 +0100

    Create rails skelleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KvbApi::Application.config.secret_token = '808482aa345180be679cfd48dba93695f7f43b5f80facbe617bd3b71faaec7fc002391fdcf0fc837dcf51e918a35afba4efc20eceb189b3ce5599bf1b0c43b3f'
