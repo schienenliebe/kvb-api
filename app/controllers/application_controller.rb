62defb96560284cbb143284c93e16f1e9f56b281
Maximilian Schulz <m.schulz@kulturfluss.de>
2013-03-05 00:07:13 +0100

    Handle dates according to gtfs standard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,16 +17,29 @@ class ApplicationController < ActionController::Base
           csv << type.gtfs_attributes.values
         end
         data.each do |row|
-          csv << row.class.gtfs_attributes.keys.map { |a| row.send(a) }
+          csv << row.class.gtfs_attributes.keys.map { |a| format_value(row.send(a)) }
         end
       end
     else
       CSV.generate(quote_char: '"', force_quotes: true) do |csv|
         csv << record.class.gtfs_attributes.values
-        csv << record.class.gtfs_attributes.keys.map { |a| record.send(a) }
+        csv << record.class.gtfs_attributes.keys.map do |attribute_name|
+          format_value(record.send(attribute_name))
+        end
       end
     end
     render text: csv_string
+  end
+
+  private
+
+  def format_value(value)
+    case value
+    when Date
+      value.strftime('%Y%m%d')
+    else
+      value
+    end
   end
 
 end
